cd4ee60077fb17a8a417a8bb4764088eeb210bc6
HilkopterBob <podewils08@gmail.com>
2024-09-14 00:21:42 +0200

    fix: naming convention

--- structs/structs.go
+++ structs/structs.go
@@ -19,34 +19,29 @@ type Package struct {
 	UpdateTime     time.Time
 }
 
-type Network_Info struct {
-	Ip_addr      string
-	Mac_addr     string
-	CreationTime time.Time
-	UpdateTime   time.Time
-}
-
 type Package_Manager struct {
-	Package_manager_name string
-	Package_repos        []string // A Slice containing all Repository Links.
-	CreationTime         time.Time
-	UpdateTime           time.Time
+	PackageManagerName string
+	PackageRepos       []string // A Slice containing all Repository Links.
+	CreationTime       time.Time
+	UpdateTime         time.Time
 }
 
 type Host struct {
 	// TODO: support different linux distros
-	Name             string // FQDN
-	ID               int
-	Current_packages []Package // A Slice with all currently installed Packages.
-	Network_info     Network_Info
-	Package_manager  Package_Manager
+	HostName       string // FQDN
+	CostID         int
+	FQDN           string
+	NetworkInfo    map[string]string //	keys: InterfaceName, IPAddress, MacAddress
+	Distro         string
+	Arch           string
+	PackageManager Package_Manager
 }
 
 type Agent struct {
-	Agent_name   string
-	Agent_secret string // a secret for encryption
-	Host_ID      int
-	Agent_ID     int
+	AgentName   string
+	AgentSecret string // a secret for encryption
+	HostID      int
+	AgentID     int
 }
 
 type ApiKey struct {
