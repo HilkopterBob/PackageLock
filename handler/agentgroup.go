e3e6c0946a466ff99fbcf684c37fca2d0cb5f254
Kartoffel096 <gollum096@gmail.com>
2024-10-08 10:48:42 +0200

    FIX: Merge artifacts

--- handler/agentgroup.go
+++ handler/agentgroup.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/base64"
 	"packagelock/db"
+	"packagelock/logger"
 	"packagelock/structs"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,39 +13,44 @@ import (
 // GetAgentByID filters a slice of Agents for a matching Agent.Agent_ID.
 // It returns a JSON response with fiber.StatusOK or fiber.StatusNotFound.
 func GetAgentByID(c *fiber.Ctx) error {
-
 	// ID is an URL slice. Its a URL-Save base64 encoded UUID
 	urlIDBytes, err := base64.RawURLEncoding.DecodeString(c.Query("id"))
 	if err != nil {
-		// FIXME: error handling
-		panic(err)
+		logger.Logger.Warnf("Can't parse AgentID from URL, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to parse AgentID.",
+		})
 	}
 
 	urlIDString := string(urlIDBytes)
 
 	agents, err := db.DB.Select("agents")
 	if err != nil {
-		// FIXME: Error handling
-		panic(err)
+		logger.Logger.Warnf("Failed to fetch 'agents' from db, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch Agents.",
+		})
 	}
 
 	var agentsSlice []structs.Agent
 	err = surrealdb.Unmarshal(agents, &agentsSlice)
 	if err != nil {
-		// FIXME: Error handling
-		panic(err)
+		logger.Logger.Warnf("Failed to unmarshal agents, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed Unmarshal.",
+		})
 	}
 
 	var requestedAgentByID structs.Agent
 	for _, agent := range agentsSlice {
 		if agent.AgentID.String() == urlIDString {
 			requestedAgentByID = agent
-			// FIXME: logging
 			return c.Status(fiber.StatusOK).JSON(requestedAgentByID)
 		}
 	}
+
+	logger.Logger.Warnf("Got Request for agent with id: %s, which dosn't exist!", urlIDString)
 	return c.Status(fiber.StatusNotFound).JSON(nil)
-	// FIXME: logging
 }
 
 // RegisterAgent handles POST requests to register a new agent.
@@ -53,8 +59,7 @@ func RegisterAgent(c *fiber.Ctx) error {
 
 	// Parse the JSON request body into newAgent
 	if err := c.BodyParser(&newAgent); err != nil {
-		// TODO: Add logs
-		// TODO: Add error handling
+		logger.Logger.Warnf("Cannot parse JSON into new Agent! Got: %s", err)
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "Cannot parse JSON",
 		})
@@ -62,53 +67,43 @@ func RegisterAgent(c *fiber.Ctx) error {
 
 	newAgentInsertionData, err := db.DB.Create("agents", newAgent)
 	if err != nil {
-		// FIXME: logging
-		// FIXME: error handling
-		panic(err)
+		logger.Logger.Warnf("Can't insert new Agent into DB, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(nil)
 	}
 	// Respond with the newly created agent
+	logger.Logger.Infof("Successfully Created new Agent with ID: %s", newAgent.AgentID)
 	return c.Status(fiber.StatusCreated).JSON(newAgentInsertionData)
 }
 
 // GetHostByAgentID finds the host for a given agent ID.
 func GetHostByAgentID(c *fiber.Ctx) error {
-
-	id, err := strconv.Atoi(c.Query("id"))
 	urlIDBytes, err := base64.RawURLEncoding.DecodeString(c.Query("id"))
 	if err != nil {
-		// FIXME: error handling
-		panic(err)
+		logger.Logger.Warnf("Can't parse AgentID from URL, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to parse AgentID.",
+		})
 	}
 
 	urlIDString := string(urlIDBytes)
 
 	agents, err := db.DB.Select("agents")
 	if err != nil {
-		// FIXME: Error handling
-		panic(err)
+		logger.Logger.Warnf("Failed to fetch 'agents' from db, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch Agents.",
+		})
 	}
 
 	var agentsSlice []structs.Agent
 	err = surrealdb.Unmarshal(agents, &agentsSlice)
 	if err != nil {
-		// FIXME: Error handling
-		panic(err)
+		logger.Logger.Warnf("Failed to unmarshal agents, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed Unmarshal.",
+		})
 	}
 
-	if len(agents) <= 0 {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "no agent with that id"})
-	}
-
-	// TODO: Filter all hosts for agent id
-	filter = bson.D{
-		{"id", agents[0].HostID},
-	}
-
-	fmt.Println(filter)
-
-	HostsCursor, err := db.Client.Database("packagelock").Collection("hosts").Find(context.Background(), filter)
-	var hosts []structs.Agent
-	if err = HostsCursor.All(context.TODO(), &hosts); err != nil {
 	var requestedAgentByID structs.Agent
 	for _, agent := range agentsSlice {
 		if agent.AgentID.String() == urlIDString {
@@ -116,7 +111,9 @@ func GetHostByAgentID(c *fiber.Ctx) error {
 		}
 	}
 
+	// if no matching agent is found, the ID is empty
 	if requestedAgentByID.ID == "" {
+		logger.Logger.Warnf("Got Request for agent with id: %s, which dosn't exist!", urlIDString)
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error": "Agent not found",
 		})
@@ -124,30 +121,30 @@ func GetHostByAgentID(c *fiber.Ctx) error {
 
 	hosts, err := db.DB.Select("hosts")
 	if err != nil {
-		// FIXME: Logging!
-		// FIXME: Error handling!
-		panic(err)
+		logger.Logger.Warnf("Failed to fetch 'hosts' from db, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch Hosts.",
+		})
 	}
 
 	var hostsSlice []structs.Host
 	err = surrealdb.Unmarshal(hosts, &hostsSlice)
 	if err != nil {
-		// FIXME: errorhandling
-		// FIXME: errorhandling
-		panic(err)
+		logger.Logger.Warnf("Failed to unmarshal hosts, got: %s", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed Unmarshal.",
+		})
 	}
 
 	var requestedHostByAgentID structs.Host
 	for _, host := range hostsSlice {
 		if host.HostID == requestedAgentByID.HostID {
 			requestedHostByAgentID = host
-			// FIXME: Logging
 			return c.Status(fiber.StatusOK).JSON(requestedHostByAgentID)
 		}
 	}
 
-	// IF HERE:
-	// No Host or No agent found
+	logger.Logger.Warnf("Agent Found, but no Host is associated... Thats Weird. AgentID: %s", requestedAgentByID.AgentID)
 	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 		"error": "Agent Found, but no Host is associated... Thats Weird.",
 	})
